bc2722e8bcd40aa4f519b1e3ef7402ae1c308cef
tigsnor <65214574+tigsnor@users.noreply.github.com>
2026-06-17 11:14:25 +0900

    Import bulk plate results from files

---
+++ lib/app/easycheck_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../features/experiments/presentation/experiments_home_screen.dart';
+import '../shared/presentation/local_data_recovery_listener.dart';
+import 'theme.dart';
+
+class EasyCheckApp extends StatelessWidget {
+  const EasyCheckApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EasyCheck',
+      debugShowCheckedModeBanner: false,
+      theme: buildEasyCheckTheme(),
+      home: const LocalDataRecoveryListener(child: ExperimentsHomeScreen()),
+    );
+  }
+}
